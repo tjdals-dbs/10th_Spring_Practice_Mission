af067d04987ef1a7666fa1bbdef1178d3a46a458
andyoon <andyoon502@naver.com>
2026-04-30 21:34:02 +0900

    Feat: 공통 응답 코드 베이스 및 일반 코드 추가

---
+++ src/main/java/umc/global/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,10 @@
+package umc.global.apiPayload.code;
+
+import org.springframework.http.HttpStatus;
+
+public interface BaseErrorCode {
+
+    HttpStatus getStatus();
+    String getCode();
+    String getMessage();
+}
---
+++ src/main/java/umc/global/apiPayload/code/GeneralSuccessCode.java
@@ -0,0 +1,20 @@
+package umc.global.apiPayload.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum GeneralSuccessCode implements BaseSuccessCode{
+
+    OK(HttpStatus.OK,
+            "COMMON200_1",
+            "성공적으로 요청을 처리했습니다"),
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
