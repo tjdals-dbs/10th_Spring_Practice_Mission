e8416fbdc56e1d985d290c9e2d102ee896ddecc7
andyoon <andyoon502@naver.com>
2026-04-30 21:44:45 +0900

    Feat: ApiResponse 추가

---
+++ src/main/java/umc/global/apiPayload/ApiResponse.java
@@ -0,0 +1,37 @@
+package umc.global.apiPayload;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import umc.global.apiPayload.code.BaseErrorCode;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+
+    @JsonProperty("code")
+    private final String code;
+
+    @JsonProperty("message")
+    private final String message;
+
+    @JsonProperty("result")
+    private T result;
+
+    //성공한 경우 (result 포함)
+    public static <T> ApiResponse<T> onSuccess(BaseErrorCode code, T result){
+        return new ApiResponse<>(true, code.getCode(), code.getMessage(), result);
+    }
+
+    // 실패한 경우 (result 포함)
+    public static <T> ApiResponse<T> onFailure(BaseErrorCode code, T result){
+        return new ApiResponse<>(false, code.getCode(), code.getMessage(), result);
+    }
+
+
+}
