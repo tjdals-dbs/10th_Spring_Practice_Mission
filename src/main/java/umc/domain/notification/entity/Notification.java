9e35e4f95ef455bbb7155088d353c6103d121e43
andyoon <andyoon502@naver.com>
2026-05-07 22:30:25 +0900

    Feat: notification 도메인 엔티티 및 enum 추가

--- src/main/java/umc/domain/notification/entity/Notification.java
+++ src/main/java/umc/domain/notification/entity/Notification.java
@@ -1,4 +1,42 @@
 package umc.domain.notification.entity;
 
-public class Notification {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import umc.domain.member.entity.Member;
+import umc.domain.notification.enums.NotificationType;
+import umc.global.entity.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "notification")
+public class Notification extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "notification_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+
+    @Column(name = "checked_at")
+    private LocalDateTime checkedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
 }
--- src/main/java/umc/domain/notification/enums/NotificationType.java
+++ src/main/java/umc/domain/notification/enums/NotificationType.java
@@ -1,4 +1,7 @@
 package umc.domain.notification.enums;
 
 public enum NotificationType {
+    EVENT,
+    REPLY_REVIEW,
+    REPLY_INQUIRY
 }
