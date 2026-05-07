84edc74a2ffe0c36b119602246578a4a95fcd23e
andyoon <andyoon502@naver.com>
2026-05-07 22:31:38 +0900

    Feat: Term 도메인 엔티티 추가

--- src/main/java/umc/domain/term/entity/Term.java
+++ src/main/java/umc/domain/term/entity/Term.java
@@ -1,4 +1,29 @@
 package umc.domain.term.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "term")
 public class Term {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "is_required", nullable = false)
+    private boolean isRequired;
+
+    @Column(name = "content", nullable = false)
+    private String content;
 }
